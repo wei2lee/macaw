4957e97baac2dec4d0bd97ceae70d666a3747fe7
Uber-jackylee <jacky@uberfusion.com>
2017-04-12 11:09:00 +0800

    [] Consider SVG root size

--- Source/model/scene/Group.swift
+++ Source/model/scene/Group.swift
@@ -1,8 +1,9 @@
 import Foundation
+import UIKit
 
 open class Group: Node {
-
-	open var contentsVar: AnimatableVariable<[Node]>
+    var userRect:CGRect? = nil
+    open var contentsVar: AnimatableVariable<[Node]>
     open var contents: [Node] {
         get { return contentsVar.value }
         set(val) {
@@ -19,45 +20,47 @@ open class Group: Node {
             }
         }
     }
-
-
-	public init(contents: [Node] = [], place: Transform = Transform.identity, opaque: Bool = true, opacity: Double = 1, clip: Locus? = nil, effect: Effect? = nil, visible: Bool = true, tag: [String] = []) {
-		self.contentsVar = AnimatableVariable<[Node]>(contents)
-		super.init(
-			place: place,
-			opaque: opaque,
-			opacity: opacity,
-			clip: clip,
-			effect: effect,
-			visible: visible,
-			tag: tag
-		)
+    
+    
+    public init(contents: [Node] = [], place: Transform = Transform.identity, opaque: Bool = true, opacity: Double = 1, clip: Locus? = nil, effect: Effect? = nil, visible: Bool = true, tag: [String] = []) {
+        self.contentsVar = AnimatableVariable<[Node]>(contents)
+        super.init(
+            place: place,
+            opaque: opaque,
+            opacity: opacity,
+            clip: clip,
+            effect: effect,
+            visible: visible,
+            tag: tag
+        )
         
         self.contentsVar.node = self
-	}
-
-	// GENERATED NOT
-	override internal func bounds() -> Rect? {
-
-		guard let firstPos = contents.first?.place else {
-			return .none
-		}
-
-		guard var union = contents.first?.bounds()?.applyTransform(firstPos) else {
-
-			return .none
-		}
-
-		contents.forEach { node in
-			guard let nodeBounds = node.bounds() else {
-				return
-			}
-
-			union = union.union(rect: nodeBounds.applyTransform(node.place))
-		}
-
-		return union
-	}
+    }
+    
+    // GENERATED NOT
+    override internal func bounds() -> Rect? {
+        if let userRect = userRect {
+            return Rect(cgRect: userRect)
+        }
+        guard let firstPos = contents.first?.place else {
+            return .none
+        }
+        
+        guard var union = contents.first?.bounds()?.applyTransform(firstPos) else {
+            
+            return .none
+        }
+        
+        contents.forEach { node in
+            guard let nodeBounds = node.bounds() else {
+                return
+            }
+            
+            union = union.union(rect: nodeBounds.applyTransform(node.place))
+        }
+        
+        return union
+    }
     
     override func shouldCheckForPressed() -> Bool {
         var shouldCheck = super.shouldCheckForPressed()
